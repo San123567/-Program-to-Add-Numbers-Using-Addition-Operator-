41675e76a8c6c49643c488f009baf6a131a5b058
San123567 <sanikasahu8@gmail.com>
2026-02-07 21:08:36 +0530

    Add user input and sum calculation in adduser.cpp

---
+++ adduser.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2, sum;
+
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;  // Taking user input
+
+    sum = num1 + num2;  // Adding numbers
+
+    cout << "Sum: " << sum << endl;  // Printing the sum
+    return 0;
+}
