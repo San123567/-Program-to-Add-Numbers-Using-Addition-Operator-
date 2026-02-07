7f18a1f82709047eca1d1a96e400fb9fd9e3b24c
San123567 <sanikasahu8@gmail.com>
2026-02-07 21:42:21 +0530

    Convert Integer to Char  Using Type Casting char()

---
+++ intchar.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    // 65 is ASCII code for 'A'
+    int num = 65;
+    
+    // Convert int to char using type casting
+    char ch = char(num);           
+    cout << "Character: " << ch;
+    return 0;
+}
