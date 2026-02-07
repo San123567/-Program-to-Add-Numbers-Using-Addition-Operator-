857011ed264e04991b1ef80397dfa80cc3985785
San123567 <sanikasahu8@gmail.com>
2026-02-07 21:31:36 +0530

    Multiplication Table Using For Loop

---
+++ multiplicationloop.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num = 7;
+
+    // loop runs from 1 to 10
+    for (int i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " 
+                        << num * i << endl;
+    }
+    return 0;
+}
